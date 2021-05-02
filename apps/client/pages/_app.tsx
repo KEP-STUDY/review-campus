32eed36956ce2eaed2b082cf50e82a200bb5b27b
BuildTheTruth <dkwk1254@naver.com>
2021-05-03 00:55:38 +0900

    Modify CustomDocument for using styled-components and material-ui in ssr

--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
+import { Header } from 'components'
+import { ThemeProvider } from 'styled-components'
+import theme from 'utils/theme'
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Head>
         <title>리뷰 캠퍼스</title>
       </Head>
       <div className="app">
+        <Header />
         <main>
           <Component {...pageProps} />
         </main>
       </div>
-    </>
+    </ThemeProvider>
   )
 }
 
---
+++ apps/client/pages/login.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Login() {
+  return <div>Login Page</div>
+}
+
+export default Login
