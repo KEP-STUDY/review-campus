ba81814e8079b70cc762df80f61d6236fdad8982
BuildTheTruth <dkwk1254@naver.com>
2021-05-09 20:01:57 +0900

    #7 Add oauth api for kakao login

--- apps/server/src/app/app.controller.ts
+++ apps/server/src/app/app.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
-
-import { AppService } from './app.service';
+import { Controller, Get, Query, Redirect } from '@nestjs/common'
+import { AppService } from './app.service'
 
 @Controller()
 export class AppController {
@@ -8,6 +7,22 @@ export class AppController {
 
   @Get()
   getData() {
-    return this.appService.getData();
+    return this.appService.getData()
+  }
+
+  @Get('/oauth')
+  @Redirect(process.env.CLIENT_URL)
+  async getOauth(@Query('code') code: string) {
+    try {
+      const tokenResponse = await this.appService.getToken(code)
+      const accessToken = tokenResponse.data.access_token
+      const resourceResponse = await this.appService.getResource(accessToken)
+      const profile = resourceResponse.data.properties
+      return {
+        url: `${process.env.CLIENT_URL}/login?nickname=${profile.nickname}&profile_image=${profile.profile_image}`,
+      }
+    } catch (err) {
+      console.log(err)
+    }
   }
 }
--- apps/server/src/app/app.service.ts
+++ apps/server/src/app/app.service.ts
@@ -1,8 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService, Injectable } from '@nestjs/common'
 
 @Injectable()
 export class AppService {
+  constructor(private http: HttpService) {}
+
   getData(): { message: string } {
-    return { message: 'Welcome to server!' };
+    return { message: 'Welcome to server!' }
+  }
+
+  async getToken(code: string) {
+    const url = `${process.env.KAUTH_URL}/oauth/token`
+    const params = new URLSearchParams()
+    params.append('grant_type', 'authorization_code')
+    params.append('client_id', process.env.APP_KEY || '')
+    params.append('redirect_uri', process.env.KAUTH_REDIRECT_URI || '')
+    params.append('code', code)
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+      },
+    }
+
+    return this.http.post(url, params, config).toPromise()
+  }
+
+  async getResource(accessToken: string) {
+    const url = `${process.env.KAPI_URL}/v2/user/me`
+
+    const config = {
+      headers: {
+        'Authorization': `Bearer ${accessToken}`,
+        'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+      },
+    }
+
+    return this.http.get(url, config).toPromise()
   }
 }
